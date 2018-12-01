6e6f11cea63040a7a173e7269a538bad4e5c468f
Zhuangfei Yang <naauaoyang@gmail.com>
2018-12-01 11:14:38 -0800

    add RecommendationEngine class

---
+++ recommendation/RecommendationEngine.py
@@ -0,0 +1,14 @@
+class RecommendationEngine:
+
+    def __init__(self, student):
+        self.student = student
+
+    def recommend_diet(self):
+        pass
+
+    def recommend_training(self):
+        pass
+
+    def recommend_goods(self):
+        pass
+
