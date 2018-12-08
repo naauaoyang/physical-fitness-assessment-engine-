3c06993337431eeed42edf9a233d3d18bf83529a
Zhuangfei Yang <naauaoyang@gmail.com>
2018-12-08 11:34:47 -0800

    add enums

---
+++ utils/enums/assessment.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Assessment(Enum):
+    EXCELLENCE = 'excellence'
+    GOOD = 'good'
+    PASS = 'pass'
+    FAIL = 'fail'
---
+++ utils/enums/five_dimension.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Dimension(Enum):
+    STRENGTH = 'strength'
+    SPEED = 'speed'
+    ENDURANCE = 'endurance'
+    COORDINATION = 'coordination'
+    FLEXIBILITY = 'flexibility'
